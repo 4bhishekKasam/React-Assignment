aada14522997b5a8dfd93d35cf0c3896ee5c7b26
Abhishek Konnur <konnurabhishek@gmail.com>
2018-06-09 18:20:30 +0530

    added details button to display repo

--- Components/UserList/UserList.js
+++ Components/UserList/UserList.js
@@ -6,27 +6,68 @@ const _baseUrl = "https://api.github.com/users/";
 
 class UserList extends Component {
 
+
+    renderStat(stat) {
+        return (
+          
+            <div class="jumbotron container">
+                <table class="table table-striped">
+                    <tbody>
+                        <tr key={stat.name}>
+                            <td>Data Label {stat.name} : {stat.value}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
+
     render() {
+
         const users = this.props.userList.map((user) => (
             <div className="container">
                 <div className="jumbotron container" key={user.login}>
-
                     <div className="row">
-                        <div className="col-sm-2">
+                        <div className="col-sm-3">
                             <img src={user.avatar_url}
                                 className="rounded-circle" alt="Cinque Terre" />
                         </div>
                         <div className="col-sm-6">
-                            <h4>  {user.login}</h4>
-                            <p>
-                                Profile URL : {user.html_url} <br />
-                                UserName : {user.followers}
-                            </p>
-
+                            <h3>  {user.login}</h3>
+                            <p>    Profile URL : {user.html_url} </p>
+                            <p>     Score : {user.score}   </p>
+                            <p>Type :  {user.type}</p>
+                        </div>
+                    </div>
+                    <div className="row pull-right">
+                        <div className="col-12">
+                            <button type="button" className="btn btn-primary btn-outline btn-lg"
+                                data-toggle="collapse" data-target="#collapseExample"
+                                aria-expanded="false" aria-controls="collapseExample">Details</button>
                         </div>
                     </div>
                 </div>
+
+                <div className="collapse" id="collapseExample">
+                    <div className="jumbotron container">
+                        <table class="table table-striped">
+                            <tbody>
+                                <tr>
+                                    <td>Data Label :</td>
+                                </tr>
+                                <tr>
+                                    <td>Data Label :</td>
+                                </tr>
+                                <tr>
+                                    <td>Data Label :</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
+
         ));
         return (
             <div className="container-fluid">
@@ -35,6 +76,7 @@ class UserList extends Component {
             </div>
         );
     }
+
 }
 
 UserList.propTypes = {
